8ab3319142ec605503d062d2f0594825b0c4cfbe
ino <87593133+inoUwU@users.noreply.github.com>
2025-01-15 22:54:32 +0900

    prerender設定を追加し、"/popular"を事前レンダリングするようにしました

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,8 @@ export default {
 	// Config options...
 	// Server-side render by default, to enable SPA mode set this to `false`
 	ssr: true,
+	// ssgにしたい場合はここに追加する
+	async prerender() {
+		return ["/popular"];
+	},
 } satisfies Config;
